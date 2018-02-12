26d560408e2eca6b1168ce01facab45284c5c604
Justine Kay <justinekay@justines-mbp.home>
2018-02-11 21:05:02 -0500

    Initial Commit

---
+++ MLPhotos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MLPhotos
+//
+//  Created by Justine Kay on 2/11/18.
+//  Copyright © 2018 Justine Kay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
